41d1bc51a362c3d0ef3ccfe206409fb6b589a45b
simonwillcock <simon@willcock.com.au>
2013-07-22 01:06:00 +0200

    Added error handling for non-existent users. Not very good though I don't think

--- Numbers/reddit.py
+++ Numbers/reddit.py
@@ -7,8 +7,41 @@ import urllib2
 base_url = "http://www.reddit.com/"
 
 def getUserData(username):
-	url = base_url + "user/" + username
-	data = json.load(urllib2.urlopen(url))
+	url = base_url + "user/" + username + ".json"
+	
+	try:
+		data = json.load(urllib2.urlopen(url))
+	except urllib2.HTTPError:
+		return {"error": "Username does not exist"}
 	return data
 
-print(getUserData(superted125))
+def getMostRecentPost(username):
+	jsonData = getUserData(username)
+	if 'error' in jsonData:
+		return jsonData
+	else:
+		post = jsonData['data']['children'][0]
+		return post
+	
+
+def getPostStats(username):
+	postData = getMostRecentPost(username)
+	if 'error' in postData:
+		return postData
+	else:
+		upvotes = postData['data']['ups']
+		downvotes = postData['data']['downs']
+		stats = {'upvotes': upvotes,'downvotes': downvotes}
+		return stats
+
+def main():
+	# print(json.dumps(getUserData("superted125"),indent=2))
+	# print(json.dumps(getMostRecentPost('superted125'),indent=2))
+	# print(getPostStats(getMostRecentPost('superted125')))
+	print(getPostStats('superted125'))
+	print(getPostStats('superted1256'))
+
+if __name__ == '__main__':
+	main()
+
+
