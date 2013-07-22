b69906d60abdd564b2e38b5346108acb6d01b48b
simonwillcock <simon@willcock.com.au>
2013-07-22 09:48:42 +0200

    Cleaned up error handling and added more stats info
    Users not found now quit the program
    Comparison results now show upvotes,downvotes
    Loop allows further comparisons without restarting program

--- Numbers/reddit.py
+++ Numbers/reddit.py
@@ -1,6 +1,7 @@
 # A program to do various things using the Reddit API
 
 # Importing modules
+import sys
 import json
 import urllib2
 
@@ -11,43 +12,49 @@ def getUserData(username):
 	
 	try:
 		data = json.load(urllib2.urlopen(url))
+		return data
 	except urllib2.HTTPError:
-		return {"error": "Username does not exist"}
-	return data
+		print("Username does not exist: %s" % username)
+		sys.exit(0)
+	
+	
 
 def getMostRecentPost(username):
 	jsonData = getUserData(username)
-	if 'error' in jsonData:
-		return jsonData
-	else:
-		post = jsonData['data']['children'][0]
-		return post
+	return jsonData['data']['children'][0]
 	
 
 def getPostStats(username):
 	postData = getMostRecentPost(username)
-	if 'error' in postData:
-		return postData
-	else:
-		upvotes = postData['data']['ups']
-		downvotes = postData['data']['downs']
-		stats = {'upvotes': upvotes,'downvotes': downvotes}
-		return stats
+	
+	upvotes = postData['data']['ups']
+	downvotes = postData['data']['downs']
+	stats = {'upvotes': upvotes,'downvotes': downvotes}
+	return stats
+
 
 def main():
-	user1 = 'superted125'
-	user2 = 'Kale21'
-	u1Stats = getPostStats(user1)
-	u2Stats = getPostStats(user2)
-	u1Karma = u1Stats['upvotes'] - u1Stats['downvotes']
-	u2Karma = u2Stats['upvotes'] - u2Stats['downvotes']
-	if u2Karma > u1Karma:
-		print("%s's most recent post had %d more karma than %s's" % (user2,(u2Karma - u1Karma),user1))
-	elif u1Karma > u2Karma:
-		print("%s's most recent post had %d more karma than %s's" % (user1,(u1Karma - u2Karma),user2))
-	else:
-		print("Both posts were equal!")
+	tryAgain = True
+	while tryAgain == True:
 
+		user1 = str(raw_input('First user: '))
+		user2 = str(raw_input('Second user: '))
+
+		u1Stats = getPostStats(user1)
+		u2Stats = getPostStats(user2)
+		u1Karma = u1Stats['upvotes'] - u1Stats['downvotes']
+		u2Karma = u2Stats['upvotes'] - u2Stats['downvotes']
+
+		if u2Karma > u1Karma:
+			print("%s's most recent post (%d|%d) had %d more karma than %s's (%d|%d)" % (user2,u2Stats['upvotes'],u2Stats['downvotes'],(u2Karma - u1Karma),user1,u1Stats['upvotes'],u1Stats['downvotes']))
+		elif u1Karma > u2Karma:
+			print("%s's most recent post (%d|%d) had %d more karma than %s's (%d|%d)" % (user1,u1Stats['upvotes'],u1Stats['downvotes'],(u1Karma - u2Karma),user2,u2Stats['upvotes'],u2Stats['downvotes']))
+		else:
+			print("Both posts were equal! (%s: %d|%d vs %s: %d|%d)" % (user1,u1Stats['upvotes'],u1Stats['downvotes'],user2,u2Stats['upvotes'],u2Stats['downvotes']))
+
+		checkTryAgain = raw_input("Try again (y/n)? ")
+		checkTryAgain.lower()
+		tryAgain = (checkTryAgain == "yes" or checkTryAgain == "y")
 
 if __name__ == '__main__':
 	main()
